75476e3d12181400bd8c484dc586cc30cc4f96a0
Mauricaba <mauricaba@gmail.com>
2023-07-06 19:23:13 -0400

    dash,footer,bas modified

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,18 @@ module.exports = {
   theme: {
     extend: {},
   },
+  colors: {
+    'darkblue': '#011b35',
+    'blue': '#003b57',
+    'cian': '#9dc3d7',
+    'lightblue': '#d2dfe6',
+    'casiwhite': '#fafefd',
+
+  },
+  fontFamily: {
+    sans: ['Graphik', 'sans-serif'],
+    serif: ['Merriweather', 'serif'],
+  },
   plugins: [
     require("flowbite/plugin"),
     require('@tailwindcss/forms'),
