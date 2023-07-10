3ff4533b868398ad6c507026fb491080fe9031cb
Mauricaba <mauricaba@gmail.com>
2023-07-09 20:02:11 -0400

    Cambio de colores y mejoras en la visual, funcionalidades y comentarios mejorados. Borre la arte de abajo del main que sobraba.

--- main.py
+++ main.py
@@ -261,36 +261,3 @@ def marcar_completada(id):
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=6500,debug=True)
-
-
-
-
-# from flask import Flask , render_template, request
-
-# import os
-
-# app = Flask(__name__)
-
-# @app.route('/')
-# def landing_page():
-#   return render_template('landing_page.html')
-
-# @app.route('/home')
-# def home():
-#   return render_template('homepage.html')
-
-# @app.route('/login')
-# def login():
-#   return render_template('login.html')
-
-# @app.route('/register')
-# def register():
-#   return render_template('register.html')
-
-# @app.route('/dashboard')
-# def dashboard():
-#   return render_template('dashboard.html')
-
-
-# if __name__ == '__main__':
-#   app.run(host='127.0.0.1', port= 6500, debug = True)
