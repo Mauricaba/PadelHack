48c46a84f4b25076cff04d812a0f609387b5c33d
Alexfernan98 <alexis.f.e98@gmail.com>
2023-07-09 21:16:44 -0400

    agregado el boton de eliminar tarea, el check box se mantiene marcado con el valor True

--- main.py
+++ main.py
@@ -257,6 +257,8 @@ def agregar_tareas():
 @login_required
 def marcar_completada(id):
     tarea = Tarea.query.get_or_404(id)
+    completa = True
+
 
     if current_user.rol != "Admin":
         return redirect(url_for('dashboard'))
@@ -264,7 +266,23 @@ def marcar_completada(id):
     tarea.completada = True
     db.session.commit()
 
-    return redirect(url_for("dashboard", tarea=tarea))
+    return redirect(url_for("dashboard", tarea=tarea, completa=completa))
+
+#Eliminar Tarea
+@app.route("/eliminar_tarea/<int:id>", methods=["GET", "POST"])
+@login_required
+def eliminar_tarea(id):
+    if current_user.rol != "Admin":
+        return redirect(url_for("dashboard"))
+
+    tarea_eliminar = Tarea.query.get_or_404(id)
+
+    #verifica si el usuario actual es el admin
+    if current_user.rol == "Admin":
+        #si esxiste el usuario lo eliminamos
+        db.session.delete(tarea_eliminar)
+        db.session.commit()
+        return redirect(url_for("dashboard"))
 
 
 #Ruta de contacto
